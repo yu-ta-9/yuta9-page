38ac2ad777c154d01b2554c8eac62605ff48828c
matsuoyuta <y.matsuo.dev@gmail.com>
2022-05-07 12:22:23 +0900

    Fix eslint setting

---
+++ .eslintrc.js
@@ -0,0 +1,118 @@
+module.exports = {
+  root: true,
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@next/next/recommended',
+    'next/core-web-vitals',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'prettier',
+  ],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint', 'react', 'unused-imports'],
+  rules: {
+    '@next/next/no-img-element': 'off', // next/imageを使用できないことでimgタグを使用したいので`off`にする
+    'import/no-default-export': 'error',
+    'newline-before-return': 'error',
+    'unused-imports/no-unused-imports': 'error',
+    'unused-imports/no-unused-vars': [
+      'error',
+      { vars: 'all', varsIgnorePattern: '^_', args: 'after-used', argsIgnorePattern: '^_' },
+    ],
+    'react/prop-types': 'off',
+    'react/react-in-jsx-scope': 'off',
+    '@typescript-eslint/no-unused-vars': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'error',
+    '@typescript-eslint/consistent-type-imports': 'error',
+    '@typescript-eslint/explicit-function-return-type': 'error', // 関数の返り値に型を付与しないとエラーになるように設定
+    '@typescript-eslint/consistent-type-definitions': ['error', 'type'], // interface ではなく type を使用しないとエラーになるように設定
+    '@typescript-eslint/ban-types': [
+      'error',
+      {
+        types: {
+          '{}': {
+            fixWith: 'object',
+            message: 'Use Record<string, unknown> or EmptyObject instead',
+          },
+          object: {
+            fixWith: 'object',
+            message: 'Use Record<string, unknown> or EmptyObject instead',
+          },
+        },
+      },
+    ],
+    'import/order': [
+      'error',
+      {
+        alphabetize: { caseInsensitive: true, order: 'asc' },
+        groups: ['builtin', 'external', 'internal', 'index', 'type', 'sibling', 'parent'],
+        'newlines-between': 'always',
+        pathGroups: [
+          {
+            group: 'internal',
+            pattern: 'src/components/**',
+            position: 'before',
+          },
+          {
+            group: 'internal',
+            pattern: 'src/constants',
+            position: 'before',
+          },
+          {
+            group: 'internal',
+            pattern: 'src/hooks/**',
+            position: 'before',
+          },
+          {
+            group: 'internal',
+            pattern: 'src/lib/**',
+            position: 'before',
+          },
+          {
+            group: 'internal',
+            pattern: 'src/provider/**',
+            position: 'before',
+          },
+          {
+            group: 'internal',
+            pattern: 'src/stores/**',
+            position: 'before',
+          },
+          {
+            group: 'internal',
+            pattern: 'src/styles/**',
+            position: 'before',
+          },
+          {
+            group: 'internal',
+            pattern: 'src/utils/**',
+            position: 'before',
+          },
+        ],
+      },
+    ],
+  },
+  overrides: [
+    {
+      files: ['*.stories.tsx', 'src/pages/**/*'],
+      rules: {
+        'import/no-default-export': 'off',
+      },
+    },
+  ],
+  settings: {
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
+      },
+      typescript: {
+        alwaysTryTypes: true,
+        config: 'tsconfig.json',
+      },
+    },
+    react: {
+      version: 'detect', // React version. "detect" automatically picks the version you have installed.
+    },
+  },
+};
