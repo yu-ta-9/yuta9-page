9f895c96811c938f9420b5dce1145f406a44a148
matsuoyuta <y.matsuo.dev@gmail.com>
2022-06-05 12:07:24 +0900

    Fix node env key

--- src/@types/global.d.ts
+++ src/@types/global.d.ts
@@ -1,5 +1,6 @@
 declare namespace NodeJS {
-  type ProcessEnv = {
-    readonly API_KEY: string;
+  // eslint-disable-next-line @typescript-eslint/consistent-type-definitions
+  interface ProcessEnv {
+    readonly NEXT_PUBLIC_API_KEY: string;
   }
 }
