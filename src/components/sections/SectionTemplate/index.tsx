78a346651c39d1df1ac24bd530a061df877dd771
matsuoyuta <y.matsuo.dev@gmail.com>
2022-06-06 09:12:15 +0900

    Small fix

--- src/components/sections/SectionTemplate/index.tsx
+++ src/components/sections/SectionTemplate/index.tsx
@@ -3,12 +3,12 @@ import styles from './style.module.scss';
 type Props = {
   children: JSX.Element;
   title?: string;
-  bgColor: string;
+  bgColor?: string;
 };
 
 export const SectionTemplate: React.FC<Props> = ({ children, title, bgColor }) => {
   return (
-    <section className={styles['section-template']} style={{ backgroundColor: bgColor }}>
+    <section className={styles['section-template']} style={{ backgroundColor: bgColor || 'transparent' }}>
       {title && <h2 className={styles['title']}>{title}</h2>}
       {children}
     </section>
