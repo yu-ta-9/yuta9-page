6653b413f1dbc6a6c1e3dcdd8dad12c8349849e2
matsuoyuta <y.matsuo.dev@gmail.com>
2022-06-14 10:22:24 +0900

    Fix top section to adapt responsive

--- src/components/sections/TopSection/index.tsx
+++ src/components/sections/TopSection/index.tsx
@@ -2,13 +2,23 @@ import Image from 'next/image';
 
 import styles from './style.module.scss';
 
+import { useDevice } from '../../../hooks/device';
+
 export const TopSection: React.FC = () => {
+  const isSp = useDevice();
+
   return (
     <section className={styles['section-top']}>
       <div className={styles['wrapper']}>
         <h1 className={styles['heading']}>YU-TA-9</h1>
         <div className={styles['bg']}>
-          <Image style={{ zIndex: -1 }} src='/images/img_hp_top.jpg' alt='top image' layout='fill' objectFit='cover' />
+          <Image
+            style={{ zIndex: -1 }}
+            src={isSp ? '/images/img_sp_hp_top.jpg' : '/images/img_hp_top.jpg'}
+            alt='top image'
+            layout='fill'
+            objectFit='cover'
+          />
         </div>
       </div>
     </section>
