3f4653ddfe0ed5d24b5b0f3d64078e963f621a72
Yuta Matsuo <y.matsuo.dev@gmail.com>
2023-03-21 11:00:04 +0900

    implement i18n

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
+import i18n from './next-i18next.config';
+
 const nextConfig = {
+  i18n,
   reactStrictMode: true,
   images: {
     domains: ['images.microcms-assets.io'],
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,10 +3,12 @@ import 'swiper/scss';
 import 'swiper/scss/navigation';
 import 'swiper/scss/pagination';
 import '../../styles/globals.scss';
+import { appWithTranslation } from 'next-i18next';
+
 import type { AppProps } from 'next/app';
 
 export const App = ({ Component, pageProps }: AppProps): JSX.Element => {
   return <Component {...pageProps} />;
 };
 
-export default App;
+export default appWithTranslation(App);
