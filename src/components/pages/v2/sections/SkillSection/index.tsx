7cd9457e5820c6e6aa9be4b2eecedd0b1f9e487b
Yuta Matsuo <y.matsuo.dev@gmail.com>
2023-04-06 09:23:55 +0900

    update skill section

--- src/components/pages/v2/sections/SkillSection/index.tsx
+++ src/components/pages/v2/sections/SkillSection/index.tsx
@@ -1,7 +1,55 @@
+import classNames from 'classnames';
+import { useState } from 'react';
+
 import type { FC } from 'react';
 
 import styles from './style.module.scss';
 
+type Stack = 'frontend' | 'backend' | 'infrastructure' | 'other';
+
+// TODO: 更新する
+const getDescription = (stack: Stack): string => {
+  switch (stack) {
+    case 'frontend':
+      return 'HTML / CSS / JavaScript / TypeScript / accessibility';
+    case 'backend':
+      return 'Java / Ruby / Ruby on Rails';
+    case 'infrastructure':
+      return 'AWS / Terraform';
+    case 'other':
+      return 'git / slack / sentry / rollbar';
+  }
+};
+
 export const SkillSection: FC = () => {
-  return <p className={styles['section-skill']}>TODO: Skill</p>;
+  const [hoverStack, setHoverStack] = useState<Stack>();
+
+  const handleMouseOver = (stack: Stack): void => {
+    setHoverStack(stack);
+  };
+
+  return (
+    <div className={styles['section-skill']}>
+      <div className={styles['graph']}>
+        <div className={classNames(styles['axis'], styles['-origin'])}></div>
+        <div className={classNames(styles['axis'], styles['-first'])}></div>
+        <div className={classNames(styles['axis'], styles['-second'])}></div>
+        <div className={classNames(styles['axis'], styles['-third'])}></div>
+        <div className={styles['frontend']} onMouseOver={(): void => handleMouseOver('frontend')}>
+          Frontend
+        </div>
+        <div className={styles['backend']} onMouseOver={(): void => handleMouseOver('backend')}>
+          Backend
+        </div>
+        <div className={styles['infrastructure']} onMouseOver={(): void => handleMouseOver('infrastructure')}>
+          Infrastructure
+        </div>
+        <div className={styles['other']} onMouseOver={(): void => handleMouseOver('other')}>
+          Other
+        </div>
+      </div>
+
+      {hoverStack !== undefined && <div className={styles['description']}>{getDescription(hoverStack)}</div>}
+    </div>
+  );
 };
