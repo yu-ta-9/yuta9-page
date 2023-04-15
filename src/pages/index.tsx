c00c8ff84c1098b3a7e740e0075727e09f91a7a0
Yuta Matsuo <y.matsuo.dev@gmail.com>
2023-04-15 23:41:59 +0900

    release new layout🔖

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,31 +1,15 @@
 import type { NextPage } from 'next';
 
-import styles from './style.module.scss';
+import V2 from './v2';
 
-import { Footer } from '../components/Footer';
-import { Header } from '../components/Header';
 import { HeadTemplate } from '../components/HeadTemplate';
-import { SECTIONS } from '../components/sections/sections';
-import { SectionTemplate } from '../components/sections/SectionTemplate';
 
-/**
- * TODO: ページ案
- * - 自作コンポーネントカタログ
- */
 const Home: NextPage = () => {
   return (
-    <div className={styles['container']}>
+    <>
       <HeadTemplate />
-      <Header />
-      <main className={styles['main']}>
-        {SECTIONS.map((section, i) => (
-          <SectionTemplate key={i} section={section.section} title={section.title} bgColor={section.bgColor}>
-            {section.content}
-          </SectionTemplate>
-        ))}
-      </main>
-      <Footer />
-    </div>
+      <V2 />
+    </>
   );
 };
 
