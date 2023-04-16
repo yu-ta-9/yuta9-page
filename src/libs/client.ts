521b4eaf1d99d802f65d7e37e4c04eeb4a47565a
Yuta Matsuo <y.matsuo.dev@gmail.com>
2023-04-17 08:13:27 +0900

    remove v1 files🔧

--- src/libs/client.ts
+++ src/libs/client.ts
@@ -1,5 +1,8 @@
 import { createClient } from 'microcms-js-sdk';
 
+/**
+ * microCMSのドメインとAPIキーを設定
+ */
 export const client = createClient({
   serviceDomain: 'yuta9-page',
   apiKey: process.env.NEXT_PUBLIC_API_KEY,
