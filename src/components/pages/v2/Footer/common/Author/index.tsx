ea2af8ef715cb0ecffe1e6204e46dd098283c8ca
Yuta Matsuo <y.matsuo.dev@gmail.com>
2023-04-22 11:22:05 +0900

    add link of YAMA page

--- src/components/pages/v2/Footer/common/Author/index.tsx
+++ src/components/pages/v2/Footer/common/Author/index.tsx
@@ -8,7 +8,12 @@ export const Author: FC = () => {
       <div>CREATED BY</div>
       <div className={styles['detail']}>
         <span className={styles['role']}>DESIGN</span>
-        <a className={styles['link']} href='#' target='_blank' rel='noreferrer'>
+        <a
+          className={styles['link']}
+          href='https://twitter.com/yanwary_life?s=21&t=wNA-HKgDfalYD-EQxz06uw'
+          target='_blank'
+          rel='noreferrer'
+        >
           YAMA
         </a>
         <span className={styles['role']}>FRONT-END</span>
