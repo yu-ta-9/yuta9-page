91a0c52a79df25d25adef1823338e21323cc01ce
Yuta Matsuo <y.matsuo.dev@gmail.com>
2023-05-19 22:18:58 +0900

    add ogp setting and icon

--- src/components/HeadTemplate/index.tsx
+++ src/components/HeadTemplate/index.tsx
@@ -5,7 +5,14 @@ export const HeadTemplate: React.FC = () => {
     <Head>
       <title>YU-TA-9&apos;s PAGE</title>
       <meta name='description' content='This is a home page of YU-TA-9.' />
-      <link rel='icon' href='/favicon.ico' />
+      <link rel='icon' href='/images/logo.png' />
+      {/* OGP */}
+      <meta property='og:url' content='https://yuta9-page.vercel.app/' />
+      <meta property='og:type' content='website' />
+      <meta property='og:title' content="YU-TA-9's PAGE" />
+      <meta property='og:description' content='This is a home page of YU-TA-9.' />
+      <meta property='og:site_name' content="YU-TA-9's PAGE" />
+      <meta property='og:image' content='/images/logo.png' />
     </Head>
   );
 };
