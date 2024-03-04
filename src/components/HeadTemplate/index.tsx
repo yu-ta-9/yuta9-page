8b0be4b7da4bdbd6cc0a49505fa8899b6f0ee753
Yuta Matsuo <y.matsuo.dev@gmail.com>
2024-03-05 07:15:45 +0900

    fix: meta tag

--- src/components/HeadTemplate/index.tsx
+++ src/components/HeadTemplate/index.tsx
@@ -7,7 +7,7 @@ export const HeadTemplate: React.FC = () => {
       <meta name='description' content='This is a home page of YU-TA-9.' />
       <link rel='icon' href='/images/logo.png' />
       {/* OGP */}
-      <meta property='og:url' content='https://yuta9-page.vercel.app/' />
+      <meta property='og:url' content='https://www.yu-ta-9.com//' />
       <meta property='og:type' content='website' />
       <meta property='og:title' content="YU-TA-9's PAGE" />
       <meta property='og:description' content='This is a home page of YU-TA-9.' />
